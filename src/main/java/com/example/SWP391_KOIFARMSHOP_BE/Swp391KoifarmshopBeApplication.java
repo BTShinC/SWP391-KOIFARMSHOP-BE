97c74da435796925c9b19d6b3f4e2abb446ea877
= <=>
2024-09-17 14:06:29 +0700

    Defaut

---
+++ src/main/java/com/example/SWP391_KOIFARMSHOP_BE/Swp391KoifarmshopBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SWP391_KOIFARMSHOP_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391KoifarmshopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swp391KoifarmshopBeApplication.class, args);
+	}
+
+}
