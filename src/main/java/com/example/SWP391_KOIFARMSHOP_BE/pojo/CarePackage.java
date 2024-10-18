91b1aeac1b2c0372f064c239280aea0adecd43a7
= <=>
2024-10-18 22:46:53 +0700

    Consingment & CarePackage

--- src/main/java/com/example/SWP391_KOIFARMSHOP_BE/pojo/CarePackage.java
+++ src/main/java/com/example/SWP391_KOIFARMSHOP_BE/pojo/CarePackage.java
@@ -5,39 +5,80 @@ import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
+import java.util.List;
 import java.util.Set;
 
-
 @Entity
-@Table (name = "Care_Package")
+@Table(name = "Care_Package")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CarePackage {
+
     @Id
     private String carePackageID;
+
     @NotBlank(message = "Package name cannot be blank")
     @Size(max = 100, message = "Package name must be less than 100 characters")
     private String packageName;
+
     @Size(max = 500, message = "Description must be less than 500 characters")
     private String description;
+
     @NotNull(message = "Price cannot be null")
     @PositiveOrZero(message = "Price must be zero or positive")
     private double price;
+
     @NotNull(message = "Duration cannot be null")
-    @Min(value = 1, message = "Duration must be at least 1 day")
+    @PositiveOrZero(message = "Duration must be zero or positive")
     private int duration;
-    @NotNull(message = "Food intake per day cannot be null")
-    @Min(value = 1, message = "Food intake per day must be at least 1")
-    private int foodIntakePerDay;
+
+    @NotNull(message = "Food Package cannot be null")
+    private String foodPackage;
+
+    @NotNull(message = "Tag cannot be null")
+    private String tag;
+
+    @NotNull(message = "Type cannot be null")
+    private String type;
+
+    // Lưu trữ JSON dưới dạng chuỗi TEXT trong MySQL
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String images; // Sẽ lưu chuỗi JSON của List<String>
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String services; // Sẽ lưu chuỗi JSON của List<String>
 
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name ="carepackage_id")
+    @JoinColumn(name = "carepackage_id")
     private Set<Product> product;
 
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name ="carepackage_id")
+    @JoinColumn(name = "carepackage_id")
     private Set<ProductCombo> productCombos;
 
+    // Phương thức để chuyển đổi danh sách thành JSON
+    @Transient
+    public List<String> getImagesAsList() {
+        return JsonUtils.fromJson(images);
+    }
+
+    @Transient
+    public void setImagesFromList(List<String> imagesList) {
+        this.images = JsonUtils.toJson(imagesList);
+    }
+
+    @Transient
+    public List<String> getServicesAsList() {
+        return JsonUtils.fromJson(services);
+    }
+
+    @Transient
+    public void setServicesFromList(List<String> servicesList) {
+        this.services = JsonUtils.toJson(servicesList);
+    }
 }
